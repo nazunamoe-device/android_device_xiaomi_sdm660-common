103039c852b362be89afd74d7699cadfbb59984f
Michael Bestas <mkbestas@lineageos.org>
2021-02-28 22:01:51 +0600

    sdm660-common: Don't build tinymix * This tool can be built manually for debugging cases,   there is no need to ship it in production
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b

--- sdm660.mk
+++ sdm660.mk
@@ -103,8 +103,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix.vendor
+    libvolumelistener
 
 # Audio Configs
 PRODUCT_COPY_FILES += \
