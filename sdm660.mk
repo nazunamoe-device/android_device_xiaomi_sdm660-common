9f8fceaed3e3ae400f153e39c85480612c323e1d
nazunamoe <nakashimayuki@kakao.com>
2021-03-04 15:26:25 +0900

    sdm660-common: Import Google camera

--- sdm660.mk
+++ sdm660.mk
@@ -39,6 +39,7 @@ endif
 
 # Inherit proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/GoogleCamera/camera.mk)
 
 # Common Tree Path
 COMMON_PATH := device/xiaomi/sdm660-common
