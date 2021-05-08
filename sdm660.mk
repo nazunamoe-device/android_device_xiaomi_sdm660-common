aed0b9f293da12249932562a2d789acd22bf000b
nazunamoe <nakashimayuki@kakao.com>
2021-05-08 16:43:35 +0900

    sdm660-common: Add back folio_daemon and hall switch

--- sdm660.mk
+++ sdm660.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# Folio
+PRODUCT_PACKAGES += \
+    folio_daemon
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
